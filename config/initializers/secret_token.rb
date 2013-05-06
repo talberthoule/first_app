444c32b57c4835d76430b7ec11eb97f303c03fda
talberthoule <talbert.houle@gmail.com>
2013-05-06 16:31:20 -0500

    First Git Upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project1::Application.config.secret_token = '94f0a46f63b296ac41bae5658809937b4c7fccc70b6f39a74bc25a63261dc971bf747036160b3cf862d7e4a77bbbda89e6e0eaf478648195838834bf7486aab7'
